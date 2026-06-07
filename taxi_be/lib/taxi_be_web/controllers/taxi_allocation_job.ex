c264588a5c380305c955a011c5790d925211301d
hope <81796354+esperanzaaroman@users.noreply.github.com>
2026-06-06 18:26:34 -0600

    loop con límite para notificar al conduc

--- taxi_be/lib/taxi_be_web/controllers/taxi_allocation_job.ex
+++ taxi_be/lib/taxi_be_web/controllers/taxi_allocation_job.ex
@@ -22,8 +22,8 @@ defmodule TaxiBeWeb.TaxiAllocationJob do
 
     taxis = Task.await(task)
 
-    {taxi, others, timer} = offer_to_next(
-      state |> Map.put(:candidates, taxis |> Enum.shuffle())
+    {taxi, others, timer, attempts} = offer_to_next(
+      state |> Map.put(:candidates, taxis |> Enum.shuffle()) |> Map.put(:attempts, 1)
     )
 
     {:noreply,
@@ -34,7 +34,7 @@ defmodule TaxiBeWeb.TaxiAllocationJob do
      |> Map.put(:attempts, 1)}
   end
 
-  def handle_info(:timeout, %{taxi: taxi} = state) do
+  def handle_info(:timeout, %{taxi: taxi, attempts: attempts} = state) do
     IO.puts("Boom!!")
     IO.inspect(state)
 
@@ -47,7 +47,7 @@ defmodule TaxiBeWeb.TaxiAllocationJob do
       )
     end
 
-    {new_taxi, others, timer} = offer_to_next(state)
+    {new_taxi, others, timer, attempts} = offer_to_next(state)
     {:noreply,
     state
     |> Map.put(:taxi, new_taxi)
@@ -70,9 +70,9 @@ defmodule TaxiBeWeb.TaxiAllocationJob do
     {:noreply, state}
   end
 
-  def handle_cast({:process_reject, _username}, %{timer: timer} = state) do
+  def handle_cast({:process_reject, _username}, %{timer: timer, attempts: attempts} = state) do
     if timer != nil, do: Process.cancel_timer(timer)
-    {taxi, others, new_timer} = offer_to_next(state)
+    {taxi, others, new_timer, attempts} = offer_to_next(state)
     {:noreply,
     state
     |> Map.put(:taxi, taxi)
@@ -82,23 +82,23 @@ defmodule TaxiBeWeb.TaxiAllocationJob do
   end
 
   # se acabaron candidatos pero aún hay intentos
-  def offer_to_next(%{request: request, candidates: [], attempts: attempts} = state) when attempts < 4 do
+  def offer_to_next(%{request: _request, candidates: [], attempts: attempts} = state) when attempts < 4 do
     new_candidates = candidate_taxis() |> Enum.shuffle()
     offer_to_next(state |> Map.put(:candidates, new_candidates) |> Map.put(:attempts, attempts + 1))
   end
 
   # se acabaron candidatos y ya se agotaron los intentos
-  def offer_to_next(%{request: %{"username" => username}, candidates: [], attempts: _attempts} = _state) do
+  def offer_to_next(%{request: %{"username" => username}, candidates: [], attempts: attempts} = _state) do
     TaxiBeWeb.Endpoint.broadcast(
       "customer:" <> username,
       "booking_request",
       %{msg: "No fue posible encontrar un taxi, intenta más tarde"}
     )
-    {nil, [], nil}
+    {nil, [], nil, attempts}
   end
 
 # hay candidatos disponibles
-  def offer_to_next(%{request: request, candidates: [taxi | others]} = _state) do
+  def offer_to_next(%{request: request, candidates: [taxi | others], attempts: attempts} = _state) do
     %{
       "pickup_address" => pickup_address,
       "dropoff_address" => dropoff_address,
@@ -115,7 +115,7 @@ defmodule TaxiBeWeb.TaxiAllocationJob do
     )
 
     timer = Process.send_after(self(), :timeout, 10_000)
-    {taxi, others, timer}
+    {taxi, others, timer, attempts}
   end
 
   def candidate_taxis() do
